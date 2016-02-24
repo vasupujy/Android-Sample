b3451d18837f943bcbf86c6f95eb298f4147f9b7
welcome <patelvasupujy@gmail.com>
2016-02-24 12:34:50 +0530

    commit helloworld

--- app/src/main/java/com/example/welcome/myapplication/MainActivity.java
+++ app/src/main/java/com/example/welcome/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Toast.makeText(MainActivity.this,"vasupujy",Toast.LENGTH_LONG).show();
     }
 
     @Override
